89dc5dfc442b2d43b91f8fa79f096ef4f97273e9
SWSpratlin <120224974+SWSpratlin@users.noreply.github.com>
2024-07-25 12:07:03 -0400

    cam handler, utils update

--- src/sarxtest/CamHandler.java
+++ src/sarxtest/CamHandler.java
@@ -1,7 +1,30 @@
 package sarxtest;
 
-
+import com.github.eduramiba.webcamcapture.drivers.NativeDriver;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+import java.util.Scanner;
 
 public class CamHandler {
+
+    public String os;
+    public String confirm;
+    public static boolean confirmed = false;
+
+    public CamHandler() throws IllegalArgumentException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Operating System. Choose from the following: ");
+        System.out.println("Pi, Mac Intel, Mac Silicon, Windows (Case Sensitive)");
+        os = scanner.nextLine();
+
+        System.out.println("You've chosen: " + os + ". Is this Correct? (Y/N)");
+        confirm = scanner.nextLine();
+
+        if (confirm.equals("Yes") || confirm.equals("Y")) {
+            confirmed = true;
+        } else throw new IllegalArgumentException("Operating System was not correctly input");
+    }
+
 
 }
